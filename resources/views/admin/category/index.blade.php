d7746ffc4bcddc14f9dd6e13c6bb02486d2a1ba4
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-15 23:54:56 -0700

    add: edit & delete buttons ( category->index.blade )

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -29,6 +29,7 @@
         <th>نام لاتین دسته</th>
         <th>دسته والد</th>
         <th>عملیات</th>
+        <th></th>
       </tr>
     </thead>
      
@@ -39,7 +40,18 @@
             <td>{{ $item->cat_name }}</td>
             <td>{{ $item->cat_ename }}</td>     
             <td>{{ $item->getParent->cat_name }}</td>       
-            <td> ویرایش | حذف </td>     
+            <td>
+                <a href="category/{{ $item->id }}/edit" class="btn btn-default"> ویرایش </a>
+                </td>
+
+                <td>
+                <form action="{{ action('admin\CategoryController@destroy', ['id' => $item->id]) }}" method="POST"  accept-charset="utf-8" class="pull-right"> <!--stack: 39790082-->
+                    {{ csrf_field() }}      
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="submit" name="submit" value="حذف" class="btn btn-default">
+                </form>
+            
+                </td>     
         </tr>
         <?php $i++; ?> 
     @endforeach
