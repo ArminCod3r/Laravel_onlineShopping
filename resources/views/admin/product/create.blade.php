ea2d88be86599b575ed02d5d63983fea2ca040db
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-19 02:07:22 -0700

    create.blade: product title & product text ( + ckeditor )

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -10,3 +10,74 @@
   افزودن محصول
 @endsection
 
+
+
+@section('content1')
+ <form action="{{ action('admin\ProductController@store') }}" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
+		{{ csrf_field() }}
+		
+
+		<div class="form-group">
+			<label for="title">نام محصول</label>
+  			<input type="text" name="title" id="title" class="form-control" value="" placeholder=""><br>
+
+  			@if($errors->has('title'))
+  				<span style="color: red;"> {{ $errors->first('title') }} </span>
+  			@endif
+		</div>
+
+		<div class="form-group">
+			<label for="article-ckeditor">توضیح</label>
+  			<input type="text" name="article-ckeditor" id="article-ckeditor" class="form-control" value="" placeholder=""><br>
+
+  			@if($errors->has('url'))
+  				<span style="color: red;"> {{ $errors->first('url') }} </span>
+  			@endif
+		</div>
+	
+		<div class="form-group">
+			<input type="file" name="img" id="img" style="display: none;" onchange="load_file(event)">
+			<img src="{{ url('images/noimage.jpg') }}" id='res_img' width="150" onclick="select_file()">
+
+			@if($errors->has('img'))
+  				<span style="color: red;"> {{ $errors->first('img') }} </span>
+  			@endif
+		</div>
+
+		<input type="submit" name="submit" value="ثبت" class="btn btn-primary">
+
+	</form>
+@endsection
+
+@section('content2')
+	<br><br><br><br><br><br>
+@endsection
+
+@section('footer')
+	<script type="text/javascript" src="{{ url('js/bootstrap-select.js') }}"></script>
+	<script type="text/javascript" src="{{ url('js/defaults-fa_IR.js') }}"></script>
+	<script>
+
+		select_file=function()
+		{
+		   document.getElementById('img').click();
+		};
+
+		load_file=function (event)
+		{
+		    var render=new FileReader;
+		    render.onload=function ()
+		    {
+		        var res_img=document.getElementById('res_img');
+		        res_img.src=render.result;
+		    };
+		    render.readAsDataURL(event.target.files[0]);
+		}
+
+	</script>
+
+	<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+@endsection
