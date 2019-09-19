b87b81e44096fcd6f19354f6712104f3f3ba70f3
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-19 00:29:45 -0700

    Model: Product (DONE)

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table='product';
+    protected $fillable=['title','code','title_url','code_url','price','discounts','view',
+    					 'text','product_status','bon','show_product','product_number',
+        				 'order_product','keywords','description','special'];
+    public $timestamps=false;
 }
