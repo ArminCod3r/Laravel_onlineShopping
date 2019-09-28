c2d8ee41844d014a65867d40a11a6c27d451bc58
ArminCod3r <Armin.MNSR@gmail.com>
2019-09-28 08:31:20 -0700

    index(product): edit & delete button (closeset section works)

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -10,13 +10,14 @@
 @endsection
 
 @section('content1')
-<a href="{{ url('admin/products/create') }}" class="btn btn-success"> افزودن اسلاید </a>
+<a href="{{ url('admin/products/create') }}" class="btn btn-success"> افزودن محصول </a>
 <br><br>
 
     <table class="table table-hover" dir="rtl" style="white-space: nowrap;">
         <thead>
           <tr>
             <th>نام</th>
+            <th>عملیات</th>
             <th>نام لاتین</th>
             <th>رنگها</th>
             <th>قیمت</th>
@@ -27,7 +28,6 @@
             <th>محصول</th>
             <th>تعداد فروش</th>
             <th>پیشنهاد ویژه</th>
-            <th>عملیات</th>
           </tr>
         </thead>
          
@@ -35,6 +35,16 @@
         @foreach($products as $item)
             <tr>
                 <td> {{ $item->title }} </td>
+                <td>
+                	<a href="slider/{{ $item->id }}/edit" class="fa fa-edit">  </a>
+                	
+                	<form action="{{ action('admin\SliderController@destroy', ['id' => $item->id]) }}" method="POST"  accept-charset="utf-8" class="pull-right"  onsubmit="return confirm('آیا قصد حذف این دسته را دارید؟')" id="removeForm"> <!--stack: 39790082-->
+                        {{ csrf_field() }}      
+                        <input type="hidden" name="_method" value="DELETE">
+                        <a id="submitBtn" class="fa fa-remove" style="color:red; cursor:pointer; margin-left:10px">  </a>
+                    </form>
+
+                </td>
                 <td> {{ $item->code }} </td>
                 <td></td>
                 <td> {{ $item->price }} </td>
@@ -44,30 +54,20 @@
                 <td> 
                 	<input type="checkbox" name="product_status"  value="1"
                         <?php if($item->product_status) { echo "checked"; }?> disabled >
-
                 </td>
 
                 <td> {{ $item->bon }} </td>
 
-                <td>
-                	<center>
-                		<input type="checkbox" name="show_product"  value="1"
-                        	<?php if($item->show_product) { echo "checked"; }?> disabled >
-                    </center>
-
+                <td align="center">
+                	<input type="checkbox" name="show_product"  value="1"
+                        <?php if($item->show_product) { echo "checked"; }?> disabled >
                 </td>
 
                 <td> <center> {{ $item->order_product }} </center> </td>
 
-                <td>
-                	<center>
-                		<input type="checkbox" name="special"  value="1"
-                        	<?php if($item->special) { echo "checked"; }?> disabled >
-                    </center>
-
-                </td>
-
-                <td>
+                <td align="center">
+                	<input type="checkbox" name="special"  value="1"
+                        <?php if($item->special) { echo "checked"; }?> disabled >
                 </td>
 
             </tr>
@@ -89,4 +89,12 @@
 
 @section('content2')
     <br><br><br><br><br><br>
+@endsection
+
+@section('footer')
+	<script type="text/javascript">
+		$("#submitBtn").bind('click', function(event) {
+		   $("#removeForm").submit();
+		});
+	</script>
 @endsection
