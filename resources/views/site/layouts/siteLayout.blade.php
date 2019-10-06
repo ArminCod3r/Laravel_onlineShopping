eba4ae3fec3a7b42f8ee3b5d89abfa0e2ebe3943
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-06 05:01:12 -0700

    Frontend: header

--- resources/views/site/layouts/siteLayout.blade.php
+++ resources/views/site/layouts/siteLayout.blade.php
@@ -10,24 +10,84 @@
 
     <!-- bootstrap + bootstrap rtl (NOTE: PRIORITY IS IMPORTANT) -->
     <link rel="stylesheet" href="{{ url('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ url('css/bootstrap-rtl.css') }}">
+    <link rel="stylesheet" href="{{ url('dist/css/bootstrap-rtl.min.css') }}">
+
+    <!-- Frontend CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ url('css/frontend.css') }}">
+
 
     <title>فروشگاه اینترنتی دیجی کالا</title>
   </head>
   <body>
 
-  <div class="container">
-    
-    <div class="row">
+  <div class="container-fluid header">
+    <div class="container">
 
-      <div class="col-md-8">
+      <div class="row">
+
+        <div class="col-md-9">
+
+          <ul class="list-inline">
+
+            <li class="list-inline-item"> 
+                <span class="fa fa-lock"></span>
+                <span>فروشگاه اینترنتی دیجی کالا</span>
+                <span> <a href="/login">وارد شوید</a> </span>
+            </li>
+
+            <li class="list-inline-item">
+              <span class="fa fa-user" ></span>
+              <span> <a href="/regsiter">ثبت نام </a> </span>
+            </li>
+
+            <li class="list-inline-item">
+              <span class="fa fa-gift" ></span>
+              <span> <a href="/gift"> کارت هدیه </a> </span>
+            </li>
+
+          </ul>
+
+          <ul class="list-inline">
+
+            <li class="list-inline-item">
+              <div class="btn-shopping-cart">
+
+                <div class="shopping-cart-icon">
+                  <span class="fa fa-shopping-cart"></span>
+                </div>
+                
+                <div class="shopping-cart-text">
+                  <span style="float: right;">سبد خرید</span>
+                  <span class="items-count-in-shopping-cart">0</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li class="list-inline-item">              
+
+              <div class="list-inline-item searching-box">
+                <div class="input-group">
+
+                  <span class="fa fa-search seach-btn"></span>
+                  <input type="text" class="form-control search-input" placeholder=" … جستجو در دیجی‌کالا">
+
+                </div>
+              </div>
+
+            </li>
+
+          </ul>
+
+        </div>
+
+        <div class="col-md-3">
+          <img src="{{ url('/images/logo.png') }}" class="logo">
+        </div>
+        
       </div>
 
-      <div class="col-md-4">
-      </div>
-      
     </div>
-
   </div>
 
   </body>
