bd67ac38b737e09f947102dc9854a8d73d6e2ef1
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-06 21:29:56 -0700

    Frontend: header (responsive)

--- resources/views/site/layouts/siteLayout.blade.php
+++ resources/views/site/layouts/siteLayout.blade.php
@@ -25,7 +25,7 @@
 
       <div class="row">
 
-        <div class="col-md-9">
+        <div class="col-md-9 col-sm-9">
 
           <ul class="list-inline">
 
@@ -81,7 +81,7 @@
 
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-3 col-sm-3">
           <img src="{{ url('/images/logo.png') }}" class="logo">
         </div>
         
