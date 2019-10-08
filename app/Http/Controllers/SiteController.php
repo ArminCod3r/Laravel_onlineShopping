c82758783953723ec9c1fb5f9a77d73bffa99af8
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-08 10:22:15 -0700

    Frontend: Recursive method is available via other methods

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,36 +8,36 @@ use DB;
 
 class SiteController extends Controller
 {
+	private $categories = array();
+
 	public function __construct()
 	{
-
+		$categories = self::categoryTree();
+		View::share('categories', $categories);
 	}
 
     public function index()
     {
-    	return view('site.index');
+    	return view('site.index'); // $this->categories
     }
 
-    public function categoryTree($parent_id = 0, $sub_mark = '')
+    // Recursive Method to get all the categories/subcategories
+    private function categoryTree($parent_id = 0, $sub_mark = '')
     {
-
 	    $query = DB::table('category')
                 ->select('*')
                 ->where('parent_id',$parent_id)
                 ->get()
-                ->pluck('cat_ename','id');
+                ->pluck('cat_name','id');
 
-        /*echo $query;
-        echo gettype($query);
-        echo $query['2'];*/
-
+        
         foreach ($query as $key => $value)
         {
-        	echo $key." : ".$sub_mark.$value."</br>";
+        	//echo $key." : ".$sub_mark.$value."</br>";
+        	array_push($this->categories, $sub_mark.$value);
         	$this->categoryTree($key, $sub_mark.'---');
         }
-
+        return $this->categories;
 	}
-
 
 }
