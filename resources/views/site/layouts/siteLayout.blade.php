a46142f80f5590076f4c7579567f100c8eb3dae0
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-09 08:19:12 -0700

    Frontend: When hovering over menu, it shows subcategories in the console.log

--- resources/views/site/layouts/siteLayout.blade.php
+++ resources/views/site/layouts/siteLayout.blade.php
@@ -1,3 +1,16 @@
+<?php
+
+$value = $categories[0];
+
+$cat_name       = explode(':', $value)[0];
+$parent_and_key = explode(':', $value)[1];
+$parent         = explode('-', $parent_and_key)[0];
+$key            = explode('-', $parent_and_key)[1];
+
+preg_match('/([a-zA-Z0-9_]*)-/', $parent_and_key, $match);
+
+?> 
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -92,10 +105,88 @@
 
 
 <div class="container-fluid menu">
-    <div class="container">
 
-    </div>
-  </div>
+  <ul class="list-inline level1-ul">
+     
+    @foreach ($categories as $key => $value)
+
+    
+        <!-- No three-lines : level1-->  
+        <?php
+          $cat_name       = explode(':', $value)[0];
+          $parent_and_key = explode(':', $value)[1];
+          $parent         = explode('-', $parent_and_key)[0];
+          $key            = explode('-', $parent_and_key)[1];
+
+          preg_match('/([a-zA-Z0-9_]*)-/', $parent_and_key, $match);
+          //echo ($match[0])
+        ?>
+        
+        @if ( preg_match('/0\b/', $value) )
+            
+          <li class="list-inline-item level1-li"
+            onmouseover="viewSubMenu('{{$cat_name}}', '{{$parent}}', '{{$key}}')"
+            onmouseout="hideSubMenu('{{$value}}')"
+            >
+            <?php echo $cat_name?>         
+        @endif
+
+        <!--
+        <ul class="list-inline">   
+          @foreach ($categories as $key2 => $value2)
+
+            @if ($key2 > $key) 
+
+              @if ( substr_count($value2, '---') == 1 )
+                  <li class="list-inline-item">
+                    {{$value2}}
+                  </li>
+              @else
+                  @break               
+              @endif
+
+            @endif
+
+          @endforeach               
+        </ul>
+      -->
+      @if ( !stristr($value, "---") )
+        </li>
+      @endif
+
+    @endforeach
+
+    </ul>
+</div>
 
   </body>
+
+  <script>
+
+    function viewSubMenu(cat_name, parent, key)
+    {
+
+      <?php foreach ($categories as $key2 => $value2): ?>
+
+        <?php
+          $cat_name_2       = explode(':', $value2)[0];
+          $parent_and_key_2 = explode(':', $value2)[1];
+          $parent_2         = explode('-', $parent_and_key_2)[0];
+          //$key_2            = explode('-', $parent_and_key)[1];
+        ?>
+
+        if (key == {!! json_encode($parent_2) !!})
+        {
+          console.log({!! json_encode($cat_name_2) !!});
+        }
+        
+      <?php endforeach ?>
+
+    }
+
+    function hideSubMenu(x)
+    {
+    }
+
+  </script>
 </html>
