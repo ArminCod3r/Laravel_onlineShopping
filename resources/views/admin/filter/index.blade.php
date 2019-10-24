d1a5b42bda3831ac488d7e8852bcd478b9348b4f
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-24 05:41:31 -0700

    Filter: view (index)

---
+++ resources/views/admin/filter/index.blade.php
@@ -0,0 +1,11 @@
+@extends('admin/layouts/adminLayout')
+
+
+@section('header')
+    <title>افزودن فیلتر</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-select.css') }}">    
+@endsection
+
+@section('custom-title')
+  افزودن فیلتر
+@endsection
