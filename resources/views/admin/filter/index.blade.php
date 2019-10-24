446e1925db1c6206fcc35444a12ec6cb005983dd
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-24 11:49:04 -0700

    Filter: parent, child

--- resources/views/admin/filter/index.blade.php
+++ resources/views/admin/filter/index.blade.php
@@ -91,21 +91,49 @@
 
         add_filter = function()
         {
+            this['count_child_for' + count] = 1;
+            //console.log('count_child_for1');
+
             var filter='<div style="margin-top:10px; margin-bottom:5px;" id="filter_parent_'+count+'">' +
 
                           '<input type="text" name="filter_name_parent[-'+count+']" class="form-control col-md-4" style="float: right" placeholder="نام فیلتر ...">'+
 
                           '<input type="text" name="filter_ename_parent[-'+count+']" class="form-control col-md-4" style="float: right" placeholder="نام لاتین فیلتر ...">'+
 
-                          '<select class="form-control col-md-4" style="margin-right:10px;">'+
+                          '<select id="parent_option_'+count+'" class="form-control col-md-4" style="margin-right:10px;">'+
                             '<option value="1"> radio </option>'+
                             '<option value="2"> color </option>'+
                           '</select>'+
-                          
+
+                          '<div id="filter_child_'+count+'" class="form-submit" style="margin:20px;">'+
+                          '</div>'+
+
+                           '<span>'+
+                             '<label class="fa fa-plus" style="color:green ; cursor:pointer" onclick="add_child(\''+count+'\')"></label>'+
+                           '</span>'+
+
                          '</div>';
             count++;
 
             $("#filters_box").append(filter);
+        }
+
+        var count_child = 1;
+        add_child = function(parent_count)
+        {
+            var selected_option = document.getElementById('parent_option_'+parent_count).value;
+            console.log(selected_option);
+
+            if (selected_option == 1)
+            {
+                var filter_child = '<input type="text" name="filter_name_child[-'+parent_count+'][-'+count_child+']" class="form-control col-md-4" style="margin-top:10px">';
+                $("#filter_child_"+parent_count).append(filter_child);
+            }
+            if (selected_option == 2)
+            {
+                alert("Nothing yet");
+            }
+            count_child++;
         }
 
     </script>
