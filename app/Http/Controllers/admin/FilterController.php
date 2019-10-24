f80cdf210936b51c9497ab101ad4b5ee0183b641
ArminCod3r <Armin.MNSR@gmail.com>
2019-10-24 06:29:54 -0700

    Filter: Showing list of categories

--- app/Http/Controllers/admin/FilterController.php
+++ app/Http/Controllers/admin/FilterController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
 
 class FilterController extends Controller
 {
+	private $categories = array();
+	
     /**
      * Display a listing of the resource.
      *
@@ -14,6 +17,29 @@ class FilterController extends Controller
      */
     public function index(Request $request)
     {
-    	return 'hi';
+    	$categories = self::categoryTree();
+
+    	return view('admin/filter/index')->with('categories', $categories);
+    }
+
+
+
+    // Recursive Method to get all the categories/subcategories
+    private function categoryTree($parent_id = 0, $sub_mark = '')
+    {
+        $query = DB::table('category')
+                ->select('*')
+                ->where('parent_id',$parent_id)
+                ->get()
+                ->pluck('cat_name','id');
+
+        
+        foreach ($query as $id => $value)
+        {
+            //echo $key." : ".$sub_mark.$value."</br>";
+            array_push($this->categories, $sub_mark.$value.':'.$id);
+            $this->categoryTree($id, $sub_mark.'---');
+        }
+        return $this->categories;
     }
 }
--- resources/views/admin/filter/index.blade.php
+++ resources/views/admin/filter/index.blade.php
@@ -3,9 +3,46 @@
 
 @section('header')
     <title>افزودن فیلتر</title>
-    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-select.css') }}">    
+    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-select.css') }}">
+    <style type="text/css">
+
+    .bootstrap-select .dropdown-toggle .filter-option
+    {
+        text-align: right;
+    }
+
+    </style>  
 @endsection
 
 @section('custom-title')
   افزودن فیلتر
+@endsection
+
+@section('content1')
+
+    <div class="form-group">
+            <label for="title">انتخاب دسته</label>
+             <select name="category" class="selectpicker selectFilterCSS" data-live-search="true" style="width: 100%; ">
+             <!--stack: 24627902-->
+
+                 <?php
+                foreach ($categories as $id=>$item)
+                {
+                    $cat_name = explode(':', $item)[0];
+                    $id       = explode(':', $item)[1];
+
+                    echo '<option value="'.$id.'">'.$cat_name.'</option>';
+                }
+             ?>              
+             </select> 
+
+        </div>
+
+@endsection
+
+
+@section('footer')
+    <script type="text/javascript" src="{{ url('js/bootstrap-select.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/defaults-fa_IR.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/jscolor.js') }}"></script>
 @endsection
