2b18ffb991bc8911e66ef075ada0a130ca218d94
ArminCod3r <Armin.MNSR@gmail.com>
2019-11-17 01:33:13 -0800

    Features: Showing list of categories

--- app/Http/Controllers/admin/FeatureController.php
+++ app/Http/Controllers/admin/FeatureController.php
@@ -4,16 +4,42 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
 
 class FeatureController extends Controller
 {
+    private $categories = array();
+
+
     public function index(Request $request)
     {
-    	return view('admin/feature/index');
+    	$categories = self::categoryTree();
+
+    	return view('admin/feature/index')->with('categories' , $categories);
     }
 
     public function create(Request $request)
     {
     	return 'create';
+    }
+
+
+    // Recursive Method to get all the categories/subcategories
+    private function categoryTree($parent_id = 0, $sub_mark = '')
+    {
+        $query = DB::table('category')
+                ->select('*')
+                ->where('parent_id',$parent_id)
+                ->get()
+                ->pluck('cat_name','id');
+
+        
+        foreach ($query as $id => $value)
+        {
+            //echo $key." : ".$sub_mark.$value."</br>";
+            array_push($this->categories, $sub_mark.$value.':'.$id);
+            $this->categoryTree($id, $sub_mark.'---');
+        }
+        return $this->categories;
     }
 }
--- resources/views/admin/feature/index.blade.php
+++ resources/views/admin/feature/index.blade.php
@@ -17,3 +17,64 @@
 @section('custom-title')
   افزودن ویژگی محصولات
 @endsection
+
+
+
+@section('content1')
+
+    @if(isset($selected_id))
+     <form action="{{ url('admin/filter?id='.$selected_id) }}" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
+     {{ csrf_field() }}
+    @endif
+
+    <div class="form-group">
+            <label for="title">انتخاب دسته</label>
+             <select name="category" class="selectpicker selectFilterCSS col-md-9" id="select_category" data-live-search="true" onchange="get_filter()">
+             <!--stack: 24627902-->
+
+                 <?php
+	                foreach ($categories as $id=>$item)
+	                {
+	                    $cat_name = explode(':', $item)[0];
+	                    $id       = explode(':', $item)[1];
+
+	                    echo '<option value="'.$id.'">'.$cat_name.'</option>';
+	                }
+	             ?>              
+             </select> 
+
+        </div>
+
+    @if(isset($selected_id))
+
+    <!-- Features will be shown here -->
+    <div class="form-group" id="filters_box">
+        
+    </div>
+
+
+    <div class="form-group" style="margin-right: 10px;">
+            <label class="fa fa-plus" style="color:red ; cursor:pointer" onclick="add_filter()"></label>
+        </div>
+
+
+     <div class="form-submit">
+         <input type="submit" name="submit" value="ثبت" class="btn btn-primary">
+     </div>
+
+     </form>
+    @endif
+
+@endsection
+
+
+@section('footer')
+    <script type="text/javascript" src="{{ url('js/bootstrap-select.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/defaults-fa_IR.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/jscolor.js') }}"></script>
+
+    <script type="text/javascript">
+        
+    </script>
+
+@endsection
