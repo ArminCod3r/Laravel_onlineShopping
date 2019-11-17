b3d5cd4b8e1444715b4f34856936422a67202976
ArminCod3r <Armin.MNSR@gmail.com>
2019-11-17 06:23:33 -0800

    Features: insert into the table

--- app/Http/Controllers/admin/FeatureController.php
+++ app/Http/Controllers/admin/FeatureController.php
@@ -33,22 +33,37 @@ class FeatureController extends Controller
     	$id = $request->get('id');
 
         $feature_names_parent  = $request->get('feature_name_parent');
-        $select_option       = $request->get('parent_option');
+        $select_option         = $request->get('parent_option');
         $feature_names_child   = $request->get('feature_name_child');
 
         foreach ($feature_names_parent as $key_parent => $value_parent)
         {
         	if($key_parent<0)
         	{
-        		echo $value_parent."<br/>";
+        		// Inserting parent-feature
+        		// Getting inserted current-id => to be: parent-id for the child-features
+        		$inserted_parent_id =  DB::table('feature')->insertGetId(
+		                                        ['category_id' => $id    ,
+		                                         'name'        => $value_parent ,
+		                                         'parent_id'   => 0 ,
+		                                         'filled'      => 1 ,
+		                                         ]
+		                                    );
 
+        		// Inserting child-features
         		if(array_key_exists($key_parent, $feature_names_child))
         		{
         			foreach ($feature_names_child[$key_parent] as $key_child => $value_child)
         			{
-        				echo $value_child."<br/>";
+        				DB::table('feature')->insert(
+		                                        ['category_id' => $id    ,
+		                                         'name'        => $value_child ,
+		                                         'parent_id'   => $inserted_parent_id ,
+		                                         'filled'      => 1 ,
+		                                         ]
+		                                    );
         			}
-        		}        		
+        		}
         	}
         }
     }
