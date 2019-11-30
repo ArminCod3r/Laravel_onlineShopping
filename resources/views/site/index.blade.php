8a9d1ea83d60f14fbbf4fa107e766118da31f816
ArminCod3r <Armin.MNSR@gmail.com>
2019-11-29 23:32:23 -0800

    Frontend: sliders-names count correction

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,7 +7,7 @@
 <div class="row">
 
 	<!-- Right Panel -->
-	<div class="col-md-3 right_panel">
+	<div class="col-sm-3 col-md-3 col-md-3 col-xl-3 right_panel">
 		<img src="{{ url('images/'.'0d72d29b.jpg') }}" >		
 		<img src="{{ url('images/'.'9de147be.jpg') }}" >		
 		<img src="{{ url('images/'.'86a4c9bc.jpg') }}" >		
@@ -15,7 +15,7 @@
 	</div>
 
 	<!-- Left Panel -->
-	<div class="col-md-9 sliders" id="sliders" class="sliders">
+	<div class="col-sm-9 col-md-9 col-lg-9 col-xl-9 sliders" id="sliders" class="sliders">
 
 		@if(sizeof($sliders)>0)
 			<div>			
@@ -38,12 +38,12 @@
 				@foreach ($sliders as $key => $item)
 
 					@if($key == 1)
-						<div class="slider_name_text slider_name_active"
+						<div class="slider_name_text slider_name_active" id="slider_text_{{ $key }}"
 												onclick="change_slider('{{ $key }}')">
 							{{ $item->title }}
 						</div>
 					@else
-						<div class="slider_name_text" id="slider_name_text"
+						<div class="slider_name_text" id="slider_text_{{ $key }}"
 							 					onclick="change_slider('{{ $key }}')"> 
 							{{ $item->title }}
 						</div>
@@ -71,19 +71,19 @@
 
 		}, 3000);
 
-
+		console.log(slide_count);
 		next = function()
 		{
-			for (var i=0 ; i<slide_count-1; i++)
+			for (var i=0 ; i<slide_count; i++)
 			{
+				console.log('slider_img_'+i);
 				document.getElementById('slider_img_'+i).style.display="none";
 			}
-
 			document.getElementById('slider_img_'+slide).style.display="block";
 
 			slide = slide+1;
 
-			if(slide == slide_count-1)
+			if(slide == slide_count)
 			{
 				slide=0;
 			}
