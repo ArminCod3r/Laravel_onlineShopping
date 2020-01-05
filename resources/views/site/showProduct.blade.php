600a07579dd67974c62d42ad4bc1dde5d02d695a
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-05 07:05:48 -0800

    Product (frontend): price

--- resources/views/site/showProduct.blade.php
+++ resources/views/site/showProduct.blade.php
@@ -112,6 +112,41 @@
 					</div>
 
 				@endif
+
+
+				<div class="price_product">
+					<?php
+
+						function toPersianNum($number)
+					    {
+					        $number = str_replace("1","۱",$number);
+					        $number = str_replace("2","۲",$number);
+					        $number = str_replace("3","۳",$number);
+					        $number = str_replace("4","۴",$number);
+					        $number = str_replace("5","۵",$number);
+					        $number = str_replace("6","۶",$number);
+					        $number = str_replace("7","۷",$number);
+					        $number = str_replace("8","۸",$number);
+					        $number = str_replace("9","۹",$number);
+					        $number = str_replace("0","۰",$number);
+
+					        return $number;
+					    }
+
+					    $product_price_persian = toPersianNum(number_format($product->price));
+
+					?>
+
+					<div class="price_org"> {{$product_price_persian}} </div>
+					<span style="color:#fb3449;"> تومان </span>
+
+
+					<input type="submit" value="افزودن به سبد خرید" class="add_cart_btn" />
+					 
+
+				</div>
+
+
 		    @endif
 
 		</div>
