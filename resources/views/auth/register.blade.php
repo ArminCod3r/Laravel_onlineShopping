5c28f9e2b21f4826095d9a198ddf8ca96d87dc34
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-10 01:30:58 -0800

    Auth (register): inputs

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,76 @@
 @extends('site/layouts/siteLayout')
 
 @section('content')
-<div class="container">
+<div class="container register">
+
+    <div class="register_header">
+        ثبت نام در دیجیکالا
+    </div>
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+        <div class="col-sm-6" style="background-color: white; border-radius: 5px">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group register_name">
+                    <label> شماره همراه یا پست الکترونیکی : </label>
+                    <input type="text" class="form-control" name="username" placeholder="Phone number or Email" />
                 </div>
-            </div>
+
+                <div class="form-group register_name">
+                    <label> کلمه عبور : </label>
+                    <input type="text" class="form-control" name="password" placeholder="Password" />
+                </div>
+
+                <div class="form-group" style="padding-top: 10px">
+                    <div class="row">
+
+                        <div class="col-sm-1">
+                        </div>
+
+                        <div class="col-sm-1">
+                            <input type="checkbox" value="1" name="privacy_check" id="privacy_check" checked="checked">
+                        </div>
+
+                        <div class="col-sm-9">
+                            <label onclick="privacy_checkbox()">
+                                حریم خصوصی و شرایط و قوانین استفاده از سرویس های سایت دیجی‌کالا را مطالعه نموده و با کلیه موارد آن موافقم.
+                            </label>
+                        </div>
+
+                        <div class="col-sm-1">
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <input type="submit" class="register_btn" value="ثبت‌نام در دیجی‌کالا" name="password"/>
+                </div>
+
+            </form>
         </div>
+
+        <div class="col-sm-6">
+        </div>
+
+        
     </div>
 </div>
+@endsection
+
+@section('footer')
+
+    <script type="text/javascript">
+
+        privacy_checkbox = function()
+        {
+            if(document.getElementById("privacy_check").checked)
+                document.getElementById("privacy_check").checked = false;
+            else
+                document.getElementById("privacy_check").checked = true;
+        }
+
+    </script>
+
 @endsection
