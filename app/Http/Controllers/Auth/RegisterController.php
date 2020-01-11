43bc67c532e30c70d5724ccdcd7993f4c418cb4e
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-11 09:10:07 -0800

    Auth (register): modified user-creations data

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -86,9 +86,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'username'  => $data['username'],
             'password_' => Hash::make($data['password_']),
+            'role'      => 'user',
         ]);
     }
 
