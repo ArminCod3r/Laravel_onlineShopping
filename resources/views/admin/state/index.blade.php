fff4910ffe517adb624a6a917021d54872fa5a5a
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-15 23:44:06 -0800

    State (backend): showing cities' in the state's index page - in the form of bootstrap select

--- resources/views/admin/state/index.blade.php
+++ resources/views/admin/state/index.blade.php
@@ -1,6 +1,7 @@
 @extends('admin/layouts/adminLayout')
 
 @section('header')
+	<link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap-select.css') }}">
 	<title>لیست استان ها</title>
 @endsection
 
@@ -15,16 +16,29 @@
 		<table class="table table-hover" style="width: 60%; margin: 20px 20px 20px 0px;">
 			<tr>
 				<th style="width: 20%;">ردیف</th>
-				<th>نام استان</th>
+				<th style="width: 50%;">نام استان</th>
 				<th>عملیات</th>
-				<th>شهرها</th>
-				<th>عنملیات</th>
 			</tr>
 
 			@foreach($states as $key=>$value)			
 				<tr>
 					<td>{{ $count }}</td>
-					<td>{{$value->name}}</td>
+					<td>
+
+					      <select class="form-control" id="cities_dropdown" class="selectpicker" data-live-search="true">
+					        <option>{{ $value->name }}</option>
+
+					    		@if( sizeof($value->city) > 0)
+									@foreach($value->city as $key_city=>$item_city)
+
+										<option value="{{$item_city->id}}" style="width: 500px;">{{$item_city->name}}</option>	
+
+									@endforeach
+								@endif
+
+					      </select>
+
+					</td>
 					<td>
 						<a href="state/{{ $value->id }}/edit" class="fa fa-edit"> </a>
 
@@ -38,30 +52,6 @@
 					<td></td>
 					<td></td>
 
-					@if( sizeof($value->city) > 0)
-	 					@foreach($value->city as $key_city=>$item_city)
-	 						<tr>
-			 					<td></td>
-			 					<td></td>
-			 					<td></td>
-			 					<td>
-			 						<div style="padding-right:20px"> {{ $item_city->name }} </div>
-			 					</td>
-			 					<td>
-			 						
-									<a href="city/{{ $item_city->id }}/edit" class="fa fa-edit"> </a>
-
-									<form action="{{ action('admin\CityController@destroy', ['id' => $item_city->id]) }}" method="POST"  accept-charset="utf-8" class="pull-right"  onsubmit="return confirm('آیا قصد حذف این دسته را دارید؟')"> <!--stack: 39790082-->
-					                        {{ csrf_field() }} 
-
-				                        <input type="hidden" name="_method" value="DELETE">
-				                        <input type="submit" name="submit" value="X" class="submitStyle">
-				                    </form>
-			 					</td>
-			 				</tr>
-	 					@endforeach
-	 				@endif
-
 				</tr>
 				<?php $count++; ?>
 			@endforeach
@@ -74,3 +64,10 @@
 	@endif
 
 @endsection
+
+
+
+@section('footer')
+	<script type="text/javascript" src="{{ url('js/bootstrap-select.js') }}"></script>
+	<script type="text/javascript" src="{{ url('js/defaults-fa_IR.js') }}"></script>
+@endsection	
