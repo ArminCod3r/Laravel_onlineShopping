c41669ac01a57711781d4dde091468d3a37f0b71
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-17 00:22:21 -0800

    Shipping (frontend): viewing shipping steps

--- resources/views/site/shipping/index.blade.php
+++ resources/views/site/shipping/index.blade.php
@@ -2,3 +2,48 @@
 
 @section('header')
 @endsection
+
+@section('content')
+
+<div class="shipping-steps">
+
+	<div class="shipping-steps-dash">
+		<div style="margin-right: 40px"></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+
+	<div class="bullet green tick">
+		<span>ورود به دیجی کالا</span>
+	</div>
+
+	<div class="shipping-line green-line"></div>
+
+
+	<div class="bullet green">
+		<span>بررسی سفارش</span>
+	</div>
+
+	<div class="shipping-line gray-line"></div>
+
+	<div class="bullet grey">
+		<span>اطلاعات ارسال</span>
+	</div>
+
+	<div class="shipping-line gray-line"></div>
+
+	<div class="bullet grey">
+		<span>اطلاعات پرداخت</span>
+	</div>
+
+	<div class="shipping-steps-dash-gray">
+		<div style="margin-right: 10px"></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	
+</div>
+
+@endsection
