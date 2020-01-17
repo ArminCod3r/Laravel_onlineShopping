485aac530846503cb41fcda6e327d39651b15e85
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-16 22:43:17 -0800

    Shipping: fetching categories from database to view on the navbar

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use View;
 
 class ShippingController extends Controller
 {
+    private $categories = array();
+
     public function __construct()
     {
         $this->middleware('auth');
+
+        $categories = self::categoryTree();
+        View::share('categories', $categories);
     }
 
     /**
@@ -18,7 +25,7 @@ class ShippingController extends Controller
      */
     public function index()
     {
-        return 'hi';
+        return view("site/shipping/index");
     }
 
     /**
@@ -85,5 +92,28 @@ class ShippingController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+
+
+
+    // Recursive Method to get all the categories/subcategories
+    private function categoryTree($parent_id = 0, $sub_mark = '')
+    {
+        $query = DB::table('category')
+                ->select('*')
+                ->where('parent_id',$parent_id)
+                ->get()
+                ->pluck('cat_name','id');
+
+        
+        foreach ($query as $key => $value)
+        {
+            //echo $key." : ".$sub_mark.$value."</br>";
+            array_push($this->categories, $value.':'.$parent_id.'-'.$key);
+            $this->categoryTree($key, $sub_mark.'---');
+        }
+        return $this->categories;
     }
 }
---
+++ resources/views/site/shipping/index.blade.php
@@ -0,0 +1,4 @@
+@extends('site/layouts/siteLayout')
+
+@section('header')
+@endsection
