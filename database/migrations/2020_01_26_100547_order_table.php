3a65cff5e8530007cb1f70646c1e38416cca4b67
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-26 02:12:31 -0800

    Created 'order' table

---
+++ database/migrations/2020_01_26_100547_order_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('address_id');
+            $table->integer('user_id');
+            $table->integer('time');
+            $table->string('date');
+            $table->smallInteger('pay_type');
+            $table->smallInteger('pay_status');
+            $table->smallInteger('order_status');
+            $table->integer('total_price');
+            $table->integer('price');
+            $table->string('code1')->nullable();
+            $table->string('code2')->nullable();
+            $table->string('order_read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
