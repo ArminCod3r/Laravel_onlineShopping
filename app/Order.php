6a1af6608bc7f84411c4ba0e92d449d20ba352e7
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-26 02:22:14 -0800

    Created 'Order' model

---
+++ app/Order.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'order';
+    public $primaryKey = 'id';
+    protected $fillable = ['address_id','user_id','time','date','pay_type','pay_status','order_status','total_price','price','code1','code2','order_read'];
+    public $timestamps = true;
+}
