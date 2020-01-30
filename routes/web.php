15cdee1bf2941c8b4540fa1a8cc4e256fd6637f6
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-30 02:20:09 -0800

    routes: middleware groups

--- routes/web.php
+++ routes/web.php
@@ -19,72 +19,86 @@ Route::get('/cart', 'SiteController@cart');
 Route::post('/cart/change', 'SiteController@cart_change');
 Route::post('/cart/count', 'SiteController@count');
 
-// Admin's idnex
-Route::get('admin', function () {
-    return view('admin.index');
+Route::middleware(['check_admin'])->group(function(){
+	// Admin's idnex
+	Route::get('admin', function () {
+	    return view('admin.index');
+	});
+
+
+	// Category
+	Route::resource('admin/category','admin\CategoryController');
+
+	// Slider
+	Route::resource('admin/slider','admin\SliderController');
+
+	// Product
+	Route::resource('admin/product', 'admin\ProductController');
+	Route::get('admin/product/gallery/{id}', 'admin\ProductController@gallery');
+	Route::post('admin/product/upload/{id}', 'admin\ProductController@upload');
+	Route::delete('admin/product/deleteImage/{img}', 'admin\ProductController@deleteImage');
+	// Except 'delete', Drop all other methods (stack:18326030)
+	Route::match(array('GET', 'POST', 'HEAD', 'PUT', 'PATCH'), 'admin/product/deleteImage/{img}', function()
+	{
+	    return abort(404);
+	});
+
+
+	// Products' Filter
+	Route::get('admin/filter' , 'admin\FilterController@index');
+	Route::post('admin/filter', 'admin\FilterController@create');
+
+	// Products' Features
+	Route::get('admin/feature' , 'admin\FeatureController@index');
+	Route::post('admin/feature', 'admin\FeatureController@create');
+	Route::get('admin/feature/list', 'admin\FeatureController@list');
+	Route::get('admin/feature/{product_id}/add', 'admin\FeatureController@add');
+	Route::get('admin/feature/{product_id}/{category_id}/add', 'admin\FeatureController@add');
+	Route::post('admin/feature/{product_id}/feature_assign', 'admin\FeatureController@feature_assign');
+
+
+	// Amazin Products
+	Route::resource('admin/amazing_products','admin\AmazingProductController');
+
+	// Review
+	//Route::resource('admin/review/{product_id}','admin\ReviewController');
+	Route::get('admin/review','admin\ReviewController@index')
+		 ->name('review.index');
+
+	Route::get('admin/review/{product_id}/create','admin\ReviewController@create' )
+		 ->name('review.create');
+
+	Route::delete('admin/review/{product_id}/destroy', 'admin\ReviewController@destroy')
+		 ->name('review.destroy');
+
+	Route::get('admin/review/{product_id}/show', 'admin\ReviewController@show')
+		 ->name('review.show');
+
+	Route::patch('admin/review/{product_id}/update', 'admin\ReviewController@update')
+		  ->name('review.update');
+
+	Route::get('admin/review/{product_id}/edit', 'admin\ReviewController@edit')
+		  ->name('review.edit');
+		  
+	Route::post('admin/review/store/{product_id}', 'admin\ReviewController@store')
+		  ->name('review.store');
+
+	Route::post('admin/review/upload/{product_id}', 'admin\ReviewController@upload');
+
+	Route::delete('admin/review/deleteImage/{img}', 'admin\ReviewController@deleteImage');
+
+
+	// state
+	Route::resource('admin/state','admin\StateController');
+
+	// city
+	Route::resource('admin/city','admin\CityController');
+
 });
 
-// Category
-Route::resource('admin/category','admin\CategoryController');
 
-// Slider
-Route::resource('admin/slider','admin\SliderController');
-
-// Product
-Route::resource('admin/product', 'admin\ProductController');
-Route::get('admin/product/gallery/{id}', 'admin\ProductController@gallery');
-Route::post('admin/product/upload/{id}', 'admin\ProductController@upload');
-Route::delete('admin/product/deleteImage/{img}', 'admin\ProductController@deleteImage');
-// Except 'delete', Drop all other methods (stack:18326030)
-Route::match(array('GET', 'POST', 'HEAD', 'PUT', 'PATCH'), 'admin/product/deleteImage/{img}', function()
-{
-    return abort(404);
-});
 
 Route::get('/categoryTree', "SiteController@categoryTree");
-
-// Products' Filter
-Route::get('admin/filter' , 'admin\FilterController@index');
-Route::post('admin/filter', 'admin\FilterController@create');
-
-// Products' Features
-Route::get('admin/feature' , 'admin\FeatureController@index');
-Route::post('admin/feature', 'admin\FeatureController@create');
-Route::get('admin/feature/list', 'admin\FeatureController@list');
-Route::get('admin/feature/{product_id}/add', 'admin\FeatureController@add');
-Route::get('admin/feature/{product_id}/{category_id}/add', 'admin\FeatureController@add');
-Route::post('admin/feature/{product_id}/feature_assign', 'admin\FeatureController@feature_assign');
-
-
-// Amazin Products
-Route::resource('admin/amazing_products','admin\AmazingProductController');
-
-// Review
-//Route::resource('admin/review/{product_id}','admin\ReviewController');
-Route::get('admin/review','admin\ReviewController@index')
-	 ->name('review.index');
-
-Route::get('admin/review/{product_id}/create','admin\ReviewController@create' )
-	 ->name('review.create');
-
-Route::delete('admin/review/{product_id}/destroy', 'admin\ReviewController@destroy')
-	 ->name('review.destroy');
-
-Route::get('admin/review/{product_id}/show', 'admin\ReviewController@show')
-	 ->name('review.show');
-
-Route::patch('admin/review/{product_id}/update', 'admin\ReviewController@update')
-	  ->name('review.update');
-
-Route::get('admin/review/{product_id}/edit', 'admin\ReviewController@edit')
-	  ->name('review.edit');
-	  
-Route::post('admin/review/store/{product_id}', 'admin\ReviewController@store')
-	  ->name('review.store');
-
-Route::post('admin/review/upload/{product_id}', 'admin\ReviewController@upload');
-
-Route::delete('admin/review/deleteImage/{img}', 'admin\ReviewController@deleteImage');
 
 
 // Auth
@@ -103,12 +117,6 @@ Route::get('captcha', function(){
 
 Route::post('if_loggedin', 'SiteController@if_loggedin');
 
-
-// state
-Route::resource('admin/state','admin\StateController');
-
-// city
-Route::resource('admin/city','admin\CityController');
 
 // shipping
 
