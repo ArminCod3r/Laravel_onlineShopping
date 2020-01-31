307ed28e78bc06d30cbb19baa96b97a07c82c58d
ArminCod3r <Armin.MNSR@gmail.com>
2020-01-31 01:02:40 -0800

    Added logout btn in the admin panel (bare minimum)

--- resources/views/admin/layouts/adminLayout.blade.php
+++ resources/views/admin/layouts/adminLayout.blade.php
@@ -170,6 +170,25 @@
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
                 class="fa fa-th-large"></i></a>
       </li>
+
+      <li class="nav-item">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->username }}
+            <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left" aria-labelledby="navbarDropdown" style="width:50%">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
+      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
