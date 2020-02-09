50a75726789cf834523bdfe7805b0f4b226eab9d
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-09 03:58:24 -0800

    Search: check database for the requested filters

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -22,44 +22,24 @@ class SearchController extends Controller
 
 
 
-    public function search(Request $request, $cat1=null, $cat2=null, $cat3=null, $cat_4=null)
+    public function search(Request $request, $cat1=null, $cat2=null, $cat3=null)
     {
-    	$cat4 = $request->get('q');
+        $filters = Filter::select('ename')->get();
 
-    	$cat1 = Category::where('cat_ename', $cat1)->firstOrFail();
+        $filter_name     = array();
+        $filters_content = array();
 
-    	$cat2 = Category::where([
-    								'cat_ename' => $cat2,
-    								'parent_id' => $cat1->id,
-    							])
-    							->firstOrFail();
+        foreach ($filters as $key => $value)
+        {
+            $url_filter = $value->ename;
 
-    	$cat3 = Category::where([
-    								'cat_ename' => $cat3,
-    								'parent_id' => $cat2->id,
-    							])
-    							->firstOrFail();
+            if($request->has($url_filter))
+            {
+                array_push($filter_name, $url_filter);
+                array_push($filters_content, $request->all($url_filter));
+            }
+        }
 
-    	$cat3_filters = array();    	
-    	if( sizeof($cat4) > 0 )
-    	{
-    		$cat3_filters = Filter::where('category_id', $cat3->id)->get();
-    	}
-
-        $cat4 = Category::where([
-                                    'cat_ename' => $cat4,
-                                    'parent_id' => $cat3->id,
-                                ])
-                                ->firstOrFail();
-
-        $products = ParentProduct::with('Product')->with('ProductImage')
-                                 ->where('parent_id', $cat4->id)
-                                 ->get();
-                                 
-
-    	return view("site/search/index")->with([
-                                                'cat3_filters'=> $cat3_filters,
-                                                'products'    => $products,
-                                              ]);
+        return [$filter_name, $filters_content];
     }
 }
