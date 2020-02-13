ca39274bbb51ff57b5b80e2a43b345409e241e73
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-13 03:46:31 -0800

    Search: changed name of the filters check-boxs AND getting JS url parameter

--- resources/views/site/search/index.blade.php
+++ resources/views/site/search/index.blade.php
@@ -36,12 +36,12 @@
 										@if((in_array(array_search($value_2->id, $brands_names), $selected_brands)))
 											<span class="filter_checkbox_true" id="{{$value_2->id}}"></span>
 
-<input type="checkbox" value="[{{$value_2->id}}_{{$value->ename}}]" checked="checked" name="checked_filters" id="checked_filters_{{$value_2->id}}" class="aaaa">
+<input type="checkbox" value="{{$value->ename}}[]={{array_search($value_2->id, $brands_names)}}" checked="checked" name="checked_filters" id="checked_filters_{{$value_2->id}}" class="aaaa">
 
 										@else
 											<span class="filter_checkbox" id="{{$value_2->id}}"></span>
 
-<input type="checkbox" value="[{{$value_2->id}}_{{$value->ename}}]" name="checked_filters" id="checked_filters_{{$value_2->id}}" class="aaaa">
+<input type="checkbox" value="{{$value->ename}}[]={{array_search($value_2->id, $brands_names)}}" name="checked_filters" id="checked_filters_{{$value_2->id}}" class="aaaa">
 
 										@endif
 										
@@ -121,14 +121,36 @@
 		}
 
 		var filter_checkbox = document.getElementsByName("checked_filters");
+		var url        = window.location.search;
+		const urlParams = new URLSearchParams(url);
 
 		for(var i=0 ; i<filter_checkbox.length ; i++)
 		{
 			if(filter_checkbox[i].checked)
 			{
 				console.log(filter_checkbox[i].value);
+
+				url_filters = window.location.search.substring(1).split("&");
+
+				for(j=0 ; j<=url_filters.length ; j++)
+				{
+					if(filter_checkbox[i].value == url_filters[j])
+					{
+						console.log("Filter: "+url_filters[j]);
+					}
+				}
+
+				// https://www.sitepoint.com/get-url-parameters-with-javascript/
+				//filters_name  = filter_checkbox[i].value.split("_")[1];
+				//filters_value = filter_checkbox[i].value.split("_")[0];
+
+				//var array_format = filters_name+'[]';
+				//const product   = urlParams.getAll(array_format);
+				//console.log(array_format);
 			}				
 		}
+
+
 
 		// filters actual checkbox which is hidden
 		/*var filter_checkbox = document.getElementsByClassName("checked_filters_class");
