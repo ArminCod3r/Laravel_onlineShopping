6b7b4375a762fa3cc395ae026416ab09a6ed54b2
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-15 05:15:45 -0800

    Search: commenting in the SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -48,17 +48,18 @@ class SearchController extends Controller
         $cat4_filters = array();
 
 
-        //$condition_values = $data['os'];
-
         $search_result = array();
         $brand  = array();
         $selected_filters = array();
 
+
+        // processing brand[] in the URL
         if(array_key_exists('brand', $data))
         {
             $brand    = $data['brand'];
             $selected_filters = $data['brand'];
 
+            // Query between tables: link_category_filter -> filter_assign -> product
             $products = DB::select("select *
                             from product
                             where id in (select product_id
@@ -68,9 +69,11 @@ class SearchController extends Controller
                                                             from link_category_filter
                                                             where category_id in (".implode(", ",$data['brand']).")))");
             
+            // pop the brand[] from the url to get the rest of filters services
             unset($data['brand']);
             $conditions = array();
 
+            // make a new array to store all the filters
             foreach ($data as $key => $value)
             {
                 foreach ($value as $key_2 => $value_2)
@@ -80,8 +83,10 @@ class SearchController extends Controller
                 }
             }
 
+            // processing the other filters in the URL
             if( sizeof($conditions) > 0 )
             {
+                // Query between tables: link_category_filter -> filter_assign -> product
                 $products_2 = DB::select("select *
                             from product
                             where id in (select product_id
@@ -91,6 +96,11 @@ class SearchController extends Controller
                                                             from link_category_filter
                                                             where category_id in (".implode(", ",$conditions).")))");
 
+                // comapring brands[]-results and the otherFilters[]-results
+                // to get the ones that are in common with each other(comapre by products' id)
+                // eg: brands[]      : 1,2,3
+                //     otherFilters[]: 2,3
+                // th result would be: 2,3
                 if( sizeof($products_2) > 0 )
                 {
                     foreach ($products as $key_1 => $value_1)
@@ -106,11 +116,13 @@ class SearchController extends Controller
                 }
             }
 
+            // if no other filter impleneted, just add the brands[] filter as the search-result
             else
                 $search_result = $products;
 
         }
 
+        // get the images of the products. array with the key of the 'product_id'
         $images = array();
         foreach ($search_result as $key => $value)
         {
@@ -122,14 +134,16 @@ class SearchController extends Controller
 
         }
 
-
+        // getting the list of the filters for the last category from the url
         $filters = Filter::where('category_id', $cat3->id)->get();
 
 
 
+        // Getting the list of the category_filters
         $linking_filters = LinkCatFilter::all();
         $linked_filters  = array();
 
+        // Changing the format of the category_filters
         foreach ($linking_filters as $key => $value)
         {
             $linked_filters[$value->category_id] = $value->filter_id;
