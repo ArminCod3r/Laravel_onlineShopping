a1a68170eb501668d1315272119a9db68a53950f
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-16 06:57:11 -0800

    Search: pagination's numbers styling

--- resources/views/include/products_list.blade.php
+++ resources/views/include/products_list.blade.php
@@ -1,3 +1,10 @@
+@if( sizeof($products) > 0 )
+
+	<div style="text-align: center">
+		{!! $products->links() !!}
+	</div>
+@endif
+
 @foreach($products as $key=>$value)
 
 	@if($key % 4 == 0)
@@ -6,24 +13,24 @@
 
 	<a href="/product/{{$value->code}}/{{$value->title_url}}">
 		<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3" style="height:750px; width: 244px">
-			<div class="product_area">
-				<div class="img">
-				<img style="width: 48%" src="{{ url('/upload/'. $images[$value->product_id]->url ) }}">
-			</div>
+		<div class="product_area">
+			<div class="img">
+			<img style="width: 48%" src="{{ url('/upload/'. $images[$value->product_id]->url ) }}">
+		</div>
 
-			<div class="text">
-				
-				<?php
-					if(strlen($value->title) > 26)
-						echo mb_substr($value->title, 0, 27, "utf-8")."...";
-					else
-						print $value->title;
-				?>
-			</div>
+		<div class="text">
+			
+			<?php
+				if(strlen($value->title) > 26)
+					echo mb_substr($value->title, 0, 27, "utf-8")."...";
+				else
+					print $value->title;
+			?>
+		</div>
 		</div>
 	</a>
 		
-						
+			<br>			
 	</div>
 
 	@if($key % 3 == 0 and $key!=0)
@@ -32,7 +39,3 @@
 
 
 @endforeach
-
-@if( sizeof($products) > 0 )
-	{{ $products->links() }}
-@endif
