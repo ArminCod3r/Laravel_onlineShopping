165d60b325d52943fadfb85c16e3231f90f746be
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-18 10:16:27 -0800

    Comment: put multiple rangeslider inside a form

--- resources/views/site/comment/show.blade.php
+++ resources/views/site/comment/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-	<div class="commenting">
+	<div class="commenting" style="margin-bottom: 30px">
 		<div class="row" style="width: 95% ; margin: auto ; background-color: white ; border-radius: 5px">
 			<div class="col-sm-5" style="text-align: center">
 				<div>
@@ -18,10 +18,55 @@
 			</div>
 
 			<div class="col-sm-7">
-				<div style="margin-top: 20px">
-					<input type="range" min="0" max="5" step="1" data-rangeslider id="range_1" />
-                    <p id="output_range_1"></p>
-				</div>
+
+				<form action="#" method="POST">
+
+					{{ csrf_field() }}
+
+					<h5 style="margin-top:20px">
+						امتیاز شما به این محصول
+					</h5>
+
+					<div style="margin-top: 30px">
+						<label>کیفیت ساخت</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_1" />
+	                    <p id="output_range_1"></p>
+					</div>
+
+					<div style="margin-top: 20px">
+						<label>ارزش خرید نسبت به قیمت</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_2" />
+	                    <p id="output_range_2"></p>
+					</div>
+
+					<div style="margin-top: 20px">
+						<label>نوآوری</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_3" />
+	                    <p id="output_range_3"></p>
+					</div>
+
+					<div style="margin-top: 20px">
+						<label>امکانات و قابلیت ها</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_4" />
+	                    <p id="output_range_4"></p>
+					</div>
+
+					<div style="margin-top: 20px">
+						<label>سهولت استفاده</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_5" />
+	                    <p id="output_range_5"></p>
+					</div>
+
+					<div style="margin-top: 20px">
+						<label>طراحی و ظاهر</label>
+						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_6" />
+	                    <p id="output_range_6"></p>
+					</div>
+
+					<input type="submit" value="ثبت" class="btn btn-primary" style="margin-bottom:30px ; width: 20% ; float:left">
+
+				</form>
+
 			</div>
 		</div>
 	</div>
