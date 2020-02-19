96e7f3ca8113629dfe5f7f0a7d8494b4eed9350a
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-19 08:28:40 -0800

    Comment: put 'form action', 'range sliders names' in the blade

--- resources/views/site/comment/show.blade.php
+++ resources/views/site/comment/show.blade.php
@@ -19,7 +19,7 @@
 
 			<div class="col-sm-7">
 
-				<form action="#" method="POST">
+				<form action="{{ action('CommentController@store_score', $product->id) }}" method="POST">
 
 					{{ csrf_field() }}
 
@@ -29,37 +29,37 @@
 
 					<div style="margin-top: 30px">
 						<label>کیفیت ساخت</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_1" />
+						<input type="range" min="0" max="5" step="1" name="score[1]" data-rangeslider id="range_1" />
 	                    <p id="output_range_1"></p>
 					</div>
 
 					<div style="margin-top: 20px">
 						<label>ارزش خرید نسبت به قیمت</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_2" />
+						<input type="range" min="0" max="5" step="1" name="score[2]" data-rangeslider id="range_2" />
 	                    <p id="output_range_2"></p>
 					</div>
 
 					<div style="margin-top: 20px">
 						<label>نوآوری</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_3" />
+						<input type="range" min="0" max="5" step="1" name="score[3]" data-rangeslider id="range_3" />
 	                    <p id="output_range_3"></p>
 					</div>
 
 					<div style="margin-top: 20px">
 						<label>امکانات و قابلیت ها</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_4" />
+						<input type="range" min="0" max="5" step="1" name="score[4]" data-rangeslider id="range_4" />
 	                    <p id="output_range_4"></p>
 					</div>
 
 					<div style="margin-top: 20px">
 						<label>سهولت استفاده</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_5" />
+						<input type="range" min="0" max="5" step="1" name="score[5]" data-rangeslider id="range_5" />
 	                    <p id="output_range_5"></p>
 					</div>
 
 					<div style="margin-top: 20px">
 						<label>طراحی و ظاهر</label>
-						<input type="range" min="0" max="5" step="1" data-rangeslider id="range_6" />
+						<input type="range" min="0" max="5" step="1" name="score[6]" data-rangeslider id="range_6" />
 	                    <p id="output_range_6"></p>
 					</div>
 
@@ -72,62 +72,65 @@
 	</div>
 
 	<div class="commenting">
+
+		<form>
 		
-		<h5> دیگران را با نوشتن نقد و بررسی و نظرات خود برای انتخاب این محصول راهنمایی کنید </h5>	
+			<h5> دیگران را با نوشتن نقد و بررسی و نظرات خود برای انتخاب این محصول راهنمایی کنید </h5>	
 
-		<div class="row title">
-			<div class="col-sm-6">
+			<div class="row title">
+				<div class="col-sm-6">
 
-				<div class="form-group">
-					<label for="title">عنوان نقد و بررسی (اجباری)</label>
-		  			<input type="text" name="title" id="title" class="form-control" value=""><br>
+					<div class="form-group">
+						<label for="title">عنوان نقد و بررسی (اجباری)</label>
+			  			<input type="text" name="title" id="title" class="form-control" value=""><br>
+					</div>
+				</div>
+
+				<div class="col-sm-6"></div>
+			</div>
+
+			<div class="row pros_cons">
+				<div class="col-sm-6">
+
+					<div class="form-group pros">
+						<p for="pros" style="color: green">نقاط قوت</p>
+			  			<input type="text" name="pros[1]" id="pros" class="form-control header" value=""><br>
+			  			<span class="fa fa-plus-circle" onclick="add_pros()"></span>
+			  			<span class="fa fa-minus-circle" onclick="remove_pros()"></span>
+
+			  			<!-- User's commented 'pros' -->
+			  			<div id="pros_area">
+
+			  			</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6">
+
+					<div class="form-group cons">
+						<p for="cons" style="color: red">نقاط ضعف</p>
+			  			<input type="text" name="cons[1]" id="cons" class="form-control header" value=""><br>
+			  			<span class="fa fa-plus-circle" onclick="add_cons()"></span>
+			  			<span class="fa fa-minus-circle" onclick="remove_cons()"></span>
+
+			  			<!-- User's commented 'cons' -->
+			  			<div id="cons_area">
+
+			  			</div>
+					</div>
 				</div>
 			</div>
 
-			<div class="col-sm-6"></div>
-		</div>
-
-		<div class="row pros_cons">
-			<div class="col-sm-6">
-
-				<div class="form-group pros">
-					<p for="pros" style="color: green">نقاط قوت</p>
-		  			<input type="text" name="pros[1]" id="pros" class="form-control header" value=""><br>
-		  			<span class="fa fa-plus-circle" onclick="add_pros()"></span>
-		  			<span class="fa fa-minus-circle" onclick="remove_pros()"></span>
-
-		  			<!-- User's commented 'pros' -->
-		  			<div id="pros_area">
-
-		  			</div>
+			<div class="row comment_text">
+				<div class="col-sm-12">
+					<div class="form-group">
+						<p for="">متن نقد و بررسی شما (اجباری)</p>
+						<textarea class="form-control"></textarea>
+					</div>
 				</div>
 			</div>
-
-			<div class="col-sm-6">
-
-				<div class="form-group cons">
-					<p for="cons" style="color: red">نقاط ضعف</p>
-		  			<input type="text" name="cons[1]" id="cons" class="form-control header" value=""><br>
-		  			<span class="fa fa-plus-circle" onclick="add_cons()"></span>
-		  			<span class="fa fa-minus-circle" onclick="remove_cons()"></span>
-
-		  			<!-- User's commented 'cons' -->
-		  			<div id="cons_area">
-
-		  			</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="row comment_text">
-			<div class="col-sm-12">
-				<div class="form-group">
-					<p for="">متن نقد و بررسی شما (اجباری)</p>
-					<textarea class="form-control"></textarea>
-				</div>
-			</div>
-		</div>
-
+		
+		</form>
 
 	</div>
 @endsection
