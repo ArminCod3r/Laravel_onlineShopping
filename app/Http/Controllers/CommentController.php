4da1c2977ff5f199993a7596020fd681fe941791
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-19 09:07:45 -0800

    Comment: storing score into the database

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\ProductScore;
 use App\Category;
 use View;
+use Auth;
 
 class CommentController extends Controller
 {
@@ -44,9 +46,40 @@ class CommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store_score(Request $request, $id)
     {
-        //
+        // Getting sent values
+        $product = Product::findOrFail($id);
+        $scores  = $request->get('score');
+
+        // Verification
+        if(is_array($scores))
+        {
+            // Duplication checking
+            $is_duplicate = ProductScore::where(['user_id'=> Auth::user()->id, 'product_id'=>$id])->get();
+
+            if( sizeof($is_duplicate) == 0 )               
+            {
+                // Store into the database
+                foreach ($scores as $key => $item)
+                {
+                    $score = new ProductScore;
+
+                    $score->product_id  = $id;
+                    $score->score_id    = $key;
+                    $score->score_value = $item;
+                    $score->user_id     = Auth::user()->id;
+
+                    $score->save();
+                }
+                return 'Done';
+            }
+            else
+                return redirect()->back();
+            
+        }
+        else
+            return redirect()->back();
     }
 
     /**
