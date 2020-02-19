6bdf8e33b00c4fd89e68fbdd4a351b511791a90f
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-19 08:55:55 -0800

    Login: fixed fetched Category from the databse

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Category;
+use App\Category;
 use View;
 use DB;
 use \Illuminate\Http\Request;
@@ -47,7 +47,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
 
-        $categories = self::categoryTree();
+        //$categories = self::categoryTree();
+        $categories = Category::all();
+        $categories = json_decode($categories, true);
+
         View::share('categories', $categories);
     }
 
