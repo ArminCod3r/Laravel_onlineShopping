1e22b5c323704df13782fde8614e1b33dd56c8f5
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-21 00:31:07 -0800

    Product (frontend): showing 'pros, 'cons', 'comment-text' in the commets-section

--- resources/views/include/comments_list.blade.php
+++ resources/views/include/comments_list.blade.php
@@ -32,7 +32,48 @@
 				</table>
 			</div>
 
-			<div class="col-sm-6"></div>
+			<div class="col-sm-6" style="border-right: 1px solid #eeeded">
+				<div>{{ $comments[$key][0]->subject }}</div>
+
+				<div style="color:green ; margin-top: 20px">
+					<span class="fa fa-arrow-up"></span>
+					نقاط قوت
+				</div>				
+
+				@if(sizeof($comments[$key])>0)
+					<?php
+						$pros_arr = explode("-::-", $comments[$key][0]->pros);								
+						$pros_arr = array_filter($pros_arr);  // Remove an empty element
+					?>
+
+					@foreach($pros_arr as $key_3=>$item)
+						<div style="margin-right: 15px"> {{$item}} </div>	  					
+	  				@endforeach
+				@endif
+
+
+
+				<div style="color:red ; margin-top: 20px">
+					<span class="fa fa-arrow-down"></span>
+					نقاط ضعف
+				</div>				
+
+				@if(sizeof($comments)>0)
+					<?php
+						$cons_arr = explode("-::-", $comments[$key][0]->cons);								
+						$cons_arr = array_filter($cons_arr);  // Remove an empty element
+					?>
+
+					@foreach($cons_arr as $key_3=>$item)
+						<div style="margin-right: 15px"> {{$item}} </div>	  					
+	  				@endforeach
+				@endif
+
+				<div class="comment_text">
+					{{ $comments[$key][0]->comment_text }}
+				</div>
+
+			</div>
 
 		</div>
 
