a736b3203dca14d2c281002dc8b88a8bf648bac3
ArminCod3r <Armin.MNSR@gmail.com>
2020-02-21 03:12:53 -0800

    relation Product<->Score to get the username of the commented person

--- app/ProductScore.php
+++ app/ProductScore.php
@@ -8,4 +8,9 @@ class ProductScore extends Model
 {
     protected $table    = 'product_score';
     protected $fillable = ['product_id', 'score_id', 'score_value', 'user_id'];
+
+    public function User()
+    {
+    	return $this->hasOne(User::class, 'id', 'user_id');
+    }    
 }
